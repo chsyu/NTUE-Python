8668558801ed0d82d4da7f95e8734aa53d7ec634
chsyu <chsyu@tea.ntue.edu.tw>
2021-10-17 06:38:18 +0800

    first commit

---
+++ HelloFastAPI/app.py
@@ -0,0 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return "Hello World ......"
+
+@app.get("/products")
+def products():
+    return {"products": {
+                "name": "book",
+                "price": 100,
+                "qty": 2
+            }}
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=5000, reload=True)
