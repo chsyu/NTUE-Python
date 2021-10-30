175fddbd99d959652f797dda8cd8a60c42c54f36
chsyu <chsyu@tea.ntue.edu.tw>
2021-10-30 14:44:05 +0800

    add comment

--- 07-SQLite_SQLAlchemy/db/database.py
+++ 07-SQLite_SQLAlchemy/db/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./shopping-cart.db"
 
+# check_same_tread: False is set for SQLite only
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
