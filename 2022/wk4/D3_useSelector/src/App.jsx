0ed0ce1b42d06a246981d50c79becc9c54808a7b
chsyu <chsyu@tea.ntue.edu.tw>
2022-10-03 10:11:36 +0800

    add store to useSelectore project

--- 2022/wk4/D3_useSelector/src/App.jsx
+++ 2022/wk4/D3_useSelector/src/App.jsx
@@ -1,28 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Product from './pages/Product'
-import Tableware from './pages/Tableware'
-import Cookware from './pages/Cookware'
-import HomeAccessories from './pages/HomeAccessories'
-import Lighting from './pages/Lighting'
-import Textile from './pages/Textile'
-import Furniture from './pages/Furniture'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Tableware from "./pages/Tableware";
+import Cookware from "./pages/Cookware";
+import HomeAccessories from "./pages/HomeAccessories";
+import Lighting from "./pages/Lighting";
+import Textile from "./pages/Textile";
+import Furniture from "./pages/Furniture";
+import store from "./redux/store";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/product/:productId" element={<Product />} />
-        
-        <Route path="/category/tableware" element={<Tableware />} />
-        <Route path="/category/cookware" element={<Cookware />} />
-        <Route path="/category/home-accessories" element={<HomeAccessories />} />
-        <Route path="/category/lighting" element={<Lighting />} />
-        <Route path="/category/textile" element={<Textile />} />
-        <Route path="/category/furniture" element={<Furniture />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:productId" element={<Product />} />
+
+          <Route path="/category/tableware" element={<Tableware />} />
+          <Route path="/category/cookware" element={<Cookware />} />
+          <Route
+            path="/category/home-accessories"
+            element={<HomeAccessories />}
+          />
+          <Route path="/category/lighting" element={<Lighting />} />
+          <Route path="/category/textile" element={<Textile />} />
+          <Route path="/category/furniture" element={<Furniture />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
