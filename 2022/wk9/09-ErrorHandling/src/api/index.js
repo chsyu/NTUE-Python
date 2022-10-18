4ff00d8058d309ccadc8d8dfd9b8b730091e28fb
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-10-18 18:05:23 +0800

    add react-query to wk9 projects

--- 2022/wk9/09-ErrorHandling/src/api/index.js
+++ 2022/wk9/09-ErrorHandling/src/api/index.js
@@ -1,46 +1,52 @@
-import axios from "axios";
-import jsonInfo from "../json/jsonInfo.json";
+import axios from "axios"
 
-const URL = "http://localhost:5000/api/v1";
+const URL = "http://127.0.0.1:5000/api/v1"
 
-export const getProductById = async (productId) => {
-  // REFERENCE PRODUCTS COLLECTION
+export const getProductById = async ({ queryKey }) => {
+  const [productId] = queryKey;
   let data = await axios.get(`${URL}/products/id/${productId}`);
   return data.data;
-};
+}
 
-export const getProducts = async (url) => {
-  const collection = jsonInfo.find((element) => element.url === url);
-  const collectionName = collection.name || "allProducts";
-  console.log(`getProducts API with url = ${collectionName}`);
+export const getProducts = async ({ queryKey }) => {
+  const [url] = queryKey;
   let data;
-  // QUERY PRODUCTS
-  if (collectionName === "allProducts")
+  if (url === "")
     data = await axios.get(`${URL}/products/all`);
-  else data = await axios.get(`${URL}/products/${collectionName}`);
-  console.log("data from API = ");
-  console.log(data);
-  return data.data;
+  else
+    data = await axios.get(`${URL}/products/${url}`);
+
+  return data;
+}
+
+export const signInWithEmailPassword = async ({ email, password }) => {
+  return await axios.post(`${URL}/users/signin`, {
+    email,
+    password,
+  });
 };
 
-export const signInWithEmailPassword = async (email, password) => {
-  try {
-    let res = await axios.post(`${URL}/users/signin`, { email, password });
-    return { status: res.status, user: res.data };
-  } catch (err) {
-    return { status: err.response.status, detail: err.response.data.detail };
-  }
+export const registerWithEmailPassword = async ({ email, password, username }) => {
+  return await axios.post(`${URL}/users/register`, {
+    email,
+    password,
+    username,
+  });
 };
 
-export const registerWithEmailPassword = async (email, password, username) => {
-  try {
-    let res = await axios.post(`${URL}/users/register`, {
-      email,
-      password,
+export const updateProfile = async ({ username, password, address, tel, access_token, user_id }) => {
+  return await axios.put(
+    `${URL}/users/update`,
+    {
+      user_id,
       username,
-    });
-    return { status: res.status, user: res.data };
-  } catch (err) {
-    return { status: err.response.status, detail: err.response.data.detail };
-  }
-};
+      password,
+      address,
+      tel,
+    },
+    {
+      headers: { Authorization: `Bearer ${access_token}` },
+    }
+  );
+}
+
