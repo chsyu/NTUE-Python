e6dd00526e2fd598d85298ab299a24d38e5fe8c6
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-11-13 16:51:07 +0800

    remove react-cookie

--- 2022/wk9/08-RegisterSigninAPI/frontend/src/App.jsx
+++ 2022/wk9/08-RegisterSigninAPI/frontend/src/App.jsx
@@ -4,8 +4,6 @@ import {
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { Provider } from "react-redux";
-import { CookiesProvider } from 'react-cookie';
-
 import Home from './pages/Home'
 import Product from './pages/Product'
 import Login from './pages/Login'
@@ -17,27 +15,24 @@ const queryClient = new QueryClient()
 
 function App() {
   return (
-    <CookiesProvider>
-      <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <BrowserRouter>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="products">
-                <Route path="category/:categoryName" element={<Home />} />
-                <Route path="id/:productId" element={<Product />} />
-              </Route>
-              <Route path="auth">
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
-                <Route path="profile" element={<Profile />} />
-              </Route>
-            </Routes>
-          </BrowserRouter>
-        </Provider>
-      </QueryClientProvider>
-    </CookiesProvider>
-
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="products">
+              <Route path="category/:categoryName" element={<Home />} />
+              <Route path="id/:productId" element={<Product />} />
+            </Route>
+            <Route path="auth">
+              <Route path="login" element={<Login />} />
+              <Route path="register" element={<Register />} />
+              <Route path="profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </QueryClientProvider>
   );
 }
 
