0862c46b91bebeeadccd1015d17c6339c075ca94
Chi-Shan Yu <chisamyu@seed.net.tw>
2022-12-12 07:09:12 +0800

    add env

--- 2022/wk12/29-1-DockerComposePostgreSQL/apiserver/db/database.py
+++ 2022/wk12/29-1-DockerComposePostgreSQL/apiserver/db/database.py
@@ -1,14 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./shopping-cart.db"
 # postgresql://${{ PGUSER }}:${{ PGPASSWORD }}@${{ PGHOST }}:${{ PGPORT }}/${{ PGDATABASE }}
+load_dotenv()
 
-
+PGUSER = os.environ.get("PGUSER")
+PGPASSWORD = os.environ.get("PGPASSWORD")
+PGHOST = os.environ.get("PGHOST")
+PGPORT = os.environ.get("PGPORT")
+PGDATABASE = os.environ.get("PGDATABASE")
 
 # SQLALCHEMY_DATABASE_URL = "postgresql://postgres:WJ9tssEgGYUSviytwMYJ@containers-us-west-58.railway.app:6472/railway"
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@0.0.0.0:5432/railway"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGDATABASE}"
 
 # engine = create_engine(
 #     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
