f7aaf72adeb374a544f70b5df943265baef9cf20
Chi-Shan Yu <chisamyu@seed.net.tw>
2023-11-28 11:57:51 +0800

    2023 wkd: with tag filter ui

---
+++ 2023/wkd/03-TagSearch/src/api/index.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const url =
+  "https://fastapi2vercel2023.vercel.app/homeworks";
+
+export const getHomeWorks = async () => {
+   try {
+      const response = await axios.get(`${url}/all`);
+      return response.data;
+   } catch (err) {
+      console.log(err);
+   }
+}
+
+export const getHomeWorksByFilter = async (filter, type) => {
+   try {
+      if (type === "semester") {
+         const response = await axios.get(`${url}/semester?semester=${filter}`);
+         return response.data;
+      } else {
+         const response = await axios.get(`${url}/school?school=${filter}`);
+         return response.data;
+      }
+   } catch (err) {
+      console.log(err);
+   }
+}
+
+export const getHomeWorkById = async (id) => {
+   try {
+      const response = await axios.get(`${url}/id?id=${id}`);
+      return response.data;
+   } catch (err) {
+      console.log(err);
+   }
+}
