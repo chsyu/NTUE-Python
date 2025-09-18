0085eeb770f0a0a9fc3274e318c0101407bba95e
Chi-Shan Yu <chisamyu@seed.net.tw>
2025-09-19 06:07:51 +0800

    add a render server example

---
+++ 2025/blog-example/backend-vercel/main.py
@@ -0,0 +1,29 @@
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from posts import posts
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/posts")
+def root():
+    return posts
+
+@app.get("/api/posts/{slug}")
+def get_post(slug: str):
+    for post in posts:
+        if post["slug"] == slug:
+            return post
+    return {"error": "Post not found"}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", port=5000, reload=True)
