296dba145f4953cc74af0bc0be0966300d296eb6
Chi-Shan Yu <chisamyu@seed.net.tw>
2025-10-22 21:39:46 +0800

    simplify nodejs and fastapi web server codes

--- 2025/3.web-server/1.nodejs-webserver/server.js
+++ 2025/3.web-server/1.nodejs-webserver/server.js
@@ -1,27 +1,16 @@
-const express = require('express');
-const http = require('http');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = 3000;  // 或其他適當的端口號
-
-
-
-// 針對根目錄的請求回覆 index.html
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-// });
 
 // 提供靜態文件
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, "dist")));
 
 // 其他所有路徑回傳 404 錯誤頁面
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, 'dist', '404.html'));
+  res.status(404).sendFile(path.join(__dirname, "dist", "404.html"));
 });
 
-
-// 啟動 HTTP 伺服器
-http.createServer(app).listen(PORT, () => {
-  console.log(`HTTP Server is running on http://localhost:${PORT}`);
-});
+app.listen(3000, () =>
+  console.log("Server is running on http://localhost:3000")
+);
--- 2025/3.web-server/3.fastapi-webserver/main.py
+++ 2025/3.web-server/3.fastapi-webserver/main.py
@@ -3,24 +3,19 @@ from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
-# === 基本設定 ===
-BASE_DIR = Path(__file__).resolve().parent
-DIST_DIR = BASE_DIR / "dist"
-PORT = 3001  
-
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory=DIST_DIR, html=True), name="static")
+# 提供靜態文件
+app.mount("/", StaticFiles(directory=Path(__file__).parent / "dist", html=True), name="static")
 
+# 其他所有路徑回傳 404 錯誤頁面
 @app.middleware("http")
-async def custom_404_middleware(request: Request, call_next):
+async def custom_404(request: Request, call_next):
     response = await call_next(request)
     if response.status_code == 404:
-        not_found = DIST_DIR / "404.html"
-        if not_found.exists():
-            return FileResponse(not_found, status_code=404, media_type="text/html")
+        return FileResponse(Path(__file__).parent / "dist" / "404.html", status_code=404)
     return response
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=PORT, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=3000)
